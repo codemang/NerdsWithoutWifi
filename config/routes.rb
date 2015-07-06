ef523a681ddb41662d14c4092cadf5e4bb5aba80
Nathaniel Rubin <nateboop@gmail.com>
2015-07-06 12:15:24 -0500

    Stable heroku build to start the app

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :trips
 
   root to: 'trips#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
