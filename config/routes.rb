b43e5252ed8c3cab14759df5a13b84b5df2edd39
codemang <nateboop@gmail.com>
2015-07-07 23:20:16 -0600

    asdf

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get '/photos' => 'photo#index'
   get '/trips/new' => 'trip#new'
+  get '/trips' => 'trips#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
