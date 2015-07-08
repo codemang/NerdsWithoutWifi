3386b056a0bef51a5a4cf5d421f58f22328e3254
codemang <nateboop@gmail.com>
2015-07-07 23:11:15 -0600

    Routing for new trip 2

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   get '/photos' => 'photo#index'
-  get '/trips/new' => 'trips#new'
+  get '/trips/new' => 'trip#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
